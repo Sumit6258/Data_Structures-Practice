00813a211dfe19a92af3572779ab4044ca9e673b
Sumit Suman <sumitsuman6258@gmail.com>
2021-08-02 13:15:41 +0530

    Add files via upload

---
+++ Array/Max_till_i_Array.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Sumit Suman on 02-08-2021.
+//
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int mx = -199999999;
+    int size;
+    cout << " Enter size of array: " ;
+    cin >> size;
+
+    int array[size];
+    cout << "Enter elements in array: ";
+    for(int i = 0; i < size; i++)
+    {
+        cin >> array[i];
+    }
+
+    for(int i = 0; i < size; i++)
+    {
+        mx = max(mx, array[i]);
+        cout << mx << endl;
+    }
+    return 0 ;
+}
+
