e6202db0d6cb01fbb4c8809d53c6f9e790a8a21c
Sumit Suman <sumitsuman6258@gmail.com>
2021-09-18 16:45:23 +0530

    Update Linked_List.c
    Linked List

--- Linked List/Linked_List.c
+++ Linked List/Linked_List.c
@@ -8,3 +8,42 @@ struct node{
     int data;
     struct node * next;
 };
+
+int main()
+{
+    struct node *head, *newnode, *temp;
+    head = 0;
+    
+    int choice;
+    while(choice)
+    {
+        newnode = (struct node *)malloc(sizeof(struct node));
+        
+        printf("Enter data:");
+        scanf("%d", &newnode -> data);
+        newnode -> next = 0;
+        
+        if(head == 0)
+        {
+            head = temp = newnode;
+        }
+        else
+        {
+            temp -> next = newnode;
+            temp = newnode;
+        }
+        
+        printf("Continue(0/1):");
+        scanf("%d", &choice);
+    }
+    
+    temp = head;
+    while(temp != 0)
+    {
+        printf("%d",temp -> data);
+        temp = temp -> next;
+    }
+    
+    return 0;
+
+}
