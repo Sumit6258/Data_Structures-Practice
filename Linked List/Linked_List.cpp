5c6e1678329c950cd345e8d2f4fa891421df9020
Sumit Suman <sumitsuman6258@gmail.com>
2021-09-27 02:08:20 +0530

    Updated linked list with more operations
    Added insert at specific position and deletion operations on linked list

--- Linked List/Linked_List.cpp
+++ Linked List/Linked_List.cpp
@@ -35,6 +35,53 @@ void insertAtTail(node* &head, int value){
     temp->next=n;
 }
 
+void insertAt(node* &head, int position, int value){
+    node* n = new node(value);
+    n -> data = value;
+    n -> next = NULL;
+
+    if(position == 1){
+        n -> next = head;
+        head = n;
+        return;
+    }
+    else if(head == NULL){
+        printf("Overflow");
+    }
+    node* n1 = head;
+    for(int i = 0; i < position - 2; i++){
+        n1 = n1 -> next;
+    }
+    n -> next = n1 -> next;
+    n1 -> next = n;
+
+}
+
+void deleteAThead(node* &head){
+    node* todelete = head;
+    head = head->next;
+
+    delete todelete;
+}
+
+void deletion(node* &head, int value){
+    if(head == NULL){
+        return;
+    }
+    if(head->next == NULL){
+        deleteAThead(head);
+        return;
+    }
+    node* temp = head;
+    while(temp->next->data!=value){
+        temp = temp->next;
+    }
+    node* todelete = temp -> next;
+    temp->next = temp->next->next;
+
+    delete todelete;
+}
+
 void display(node* head){
     node* temp = head;
     while(temp != NULL){
@@ -61,10 +108,17 @@ int main()
     insertAtTail(head, 1);
     insertAtTail(head,3);
     insertAtTail(head,5);
+    display(head);//Display all entered elements
+    insertAtTail(head,7);
     display(head);
-    insertAthead(head,7);
+    insertAthead(head,9);
+    insertAt(head,4,4);
     display(head);
-    cout<<search(head,3);
+    deletion(head,3);
+    //cout<< sumit6258 <<search(head,2);
+    display(head);
+    deleteAThead(head);
+    display(head);
 
     return 0;
 }
