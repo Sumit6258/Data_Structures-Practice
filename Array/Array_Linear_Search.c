1a8ef5436bee460597ca0c4850b9af3e9b520c6d
Sumit Suman <sumitsuman6258@gmail.com>
2021-11-10 15:14:49 +0530

    Update Array_Linear_Search.c
    Linear Searching in array in C

--- Array/Array_Linear_Search.c
+++ Array/Array_Linear_Search.c
@@ -1,5 +1,5 @@
 //
-// Created by Sumit Suman on 10-11-2021. ✌✌✌
+// Created by Sumit Suman on 10-11-2021. ✌
 //
 
 #include<stdio.h>
@@ -27,14 +27,17 @@ int main()
     {
         scanf("%d", &array[i]);
     }
+    
     int key;
     printf("Enter key element to find: ");
     scanf("%d",&key);
+    
     if(LinearSearch(array,size,key) == -1){
         printf("Element not found in the array");
     }
     else{
         printf("Element %d found at index %d" ,key,LinearSearch(array,size,key));
     }
+    
     return 0;
 }
