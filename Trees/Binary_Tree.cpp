bfe1b75c6c0f4954dfb8bfadbd00c8c4ea080235
Sumit Suman <sumitsuman6258@gmail.com>
2022-01-11 16:50:05 +0530

    Trees Data Structure

---
+++ Trees/Binary_Tree.cpp
@@ -0,0 +1,40 @@
+//
+// Created by Sumit Suman on 23-10-2021.
+//
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int main()
+{
+    struct Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+
+    return 0;
+}
+
+
+
+
+/*
+ *       1
+ *     /   \
+ *    2     3
+ */
