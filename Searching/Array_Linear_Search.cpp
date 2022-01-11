1904cc5cbac9aa436cef8d257d0a8dc3fe8dd875
Sumit Suman <sumitsuman6258@gmail.com>
2022-01-11 17:07:37 +0530

    Linear search in C++
    Linear Search in C++

--- Searching/Array_Linear_Search.cpp
+++ Searching/Array_Linear_Search.cpp
@@ -1,6 +1,6 @@
 //
 // Created by Sumit Suman on 16-07-2021.
-//
+//#
 
 #include<iostream>
 using namespace std;
@@ -23,15 +23,19 @@ int main()
     cin >> size;
 
     int array[size];
+    
     cout << "Enter elements in array: ";
     for(int i = 0; i < size; i++)
     {
         cin >> array[i];
     }
+    
     int key;
     cout << "Enter key element to find: ";
     cin >> key;
+    
     cout << "Element " <<"\""<< key <<"\"" << " found at index " <<LinearSearch(array,size,key) << endl;
+    
     return 0;
 }
 
